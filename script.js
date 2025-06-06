b83eeae04d4ea715229e30735845f341066dd5d2
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-06-06 21:46:31 +0100

    Use double quotes instead of single quotes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function download(filetype) {
     const image = canvas.toDataURL(`image/${filetype}`);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = image;
     link.download = `canvas-image.${filetype}`;
     link.click();
